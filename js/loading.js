4877b61dde4e11742722be1a5943bbdfabc1e211
YowRu <32123611+YowRu@users.noreply.github.com>
2024-03-14 15:14:31 +0800

    Add files via upload

--- js/loading.js
+++ js/loading.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-//   let pathname = window.location.pathname;
-//   let consult = pathname.split("/");
-//   let number = consult.length
 
   let words=$('title').text();
     $("header").load("header.html");
-    $('.pagination').load('pagination.html');
+    $('.keywordSearch').load('search.html');
     $('#main .content>.title').text(words);
     
 //datePicker
