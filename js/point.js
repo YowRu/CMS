609962d9b18b5f2d3e7e72494623a35d5488dde7
YowRu <32123611+YowRu@users.noreply.github.com>
2024-05-16 09:26:27 +0800

    Add files via upload

--- js/point.js
+++ js/point.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {  
 //table height
 let contentH = $('#main .accordion').height();
+let tabsWP_height = $('.tabsWP').height();
 $('.table-responsive .tableWp').height(contentH - 210);
-
+$('#tab0, #tab1, #tab4').height( contentH-tabsWP_height+19);
+$('#tab0, #tab1, #tab4').addClass('scroll');
 });
